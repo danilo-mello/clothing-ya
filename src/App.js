ac902f72d279114fc92ac4f5ea5846c008c024fb
danilo-mello <dmello19@gmail.com>
2021-04-14 14:24:00 -0700

    add signup, signin functions

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component';
-import Header from './components/header/header.component';
-import ShopPage from './pages/shop/shop.component'
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import Header from './components/header/header.component'
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.util'
 
-
-const Page404 = () => (
+const PageNotFound = () => (
   <div>
-    <h1>404 Page not found</h1>
-  </div>
-)
-
-const HatsPage = () => (
-  <div>
-    <h1>HatsPage</h1>
+    <h1>Page Not Found</h1>
   </div>
 )
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+  
+  useEffect(() => {
+    let unsubscribeFromAuth = null
+
+    unsubscribeFromAuth = auth.onAuthStateChanged( async (userAuth) => {
+      // console.log(userAuth)
+
+      const userRef = await createUserProfileDocument(userAuth)
+
+      if(userAuth){
+        userRef.onSnapshot((snapShot) => {
+          // console.log(snapShot)
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          })
+        })
+      }else{
+        setCurrentUser(userAuth)
+      }
+    })
+
+    //componentDidUnmount
+    //cleanup function
+    return () => {
+      unsubscribeFromAuth()
+    }
+  }, [])
+
   return (
     <div className="App">
-      <Header />
+      <Header currentUser={currentUser} />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInAndSignUp} />
-        <Route component={Page404} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" render={() => currentUser ? (<Redirect to="/" />) : (<SignInAndSignUp />)} />
+        <Route component={PageNotFound} />
       </Switch>
     </div>
   );
