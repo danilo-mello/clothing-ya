6a7dea19d5b3c6378012dce5a595a3cf037d77a4
danilo-mello <dmello19@gmail.com>
2021-04-21 13:57:39 -0700

    add graphQL

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,16 +6,17 @@ import './collection-preview.styles.scss'
 import CollectionItem from '../collection-item/collection-item.component'
 
 const CollectionPreview = ({ title, items, history, routeName, match }) => (
-    <div className="collection-preview">
-        <h1 className="title" onClick={() => history.push(`${match.path}/${routeName}`)}>{title && title.toUpperCase()}</h1>
-        <div className="preview">
-            {
-                items && items.filter((item, index) => index < 4).map(( item ) => (
-                    <CollectionItem key={item.id} item={item} />
-                ))
-            }
+        <div className="collection-preview">
+            <h1 className="title" onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}>{title && title.toUpperCase()}</h1>
+            <div className="preview">
+                {
+                    items && items.filter((item, index) => index < 4).map(( item ) => (
+                        <CollectionItem key={item.id} item={item} />
+                    ))
+                }
+            </div>
         </div>
-    </div>
 )
+
 
 export default withRouter(CollectionPreview)
--- src/pages/collection/collection-page.component.jsx
+++ src/pages/collection/collection-page.component.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
 import { CollectionPageContainer, CollectionTitle, CollectionItemsContainer} from './collection-page.styles'
 
 import CollectionItem from '../../components/collection-item/collection-item.component'
 
-import { selectCollection } from '../../redux/shop/shop.selector'
 
 const CollectionPage = ({ collections}) => {
     const {title, items} = collections
@@ -21,8 +19,4 @@ const CollectionPage = ({ collections}) => {
     )
 }
 
-const mapStateToProps = (state, ownProps) => ({
-    collections: selectCollection(ownProps.match.params.collectionId)(state)
-})
-
-export default connect(mapStateToProps)(CollectionPage)
+export default CollectionPage
